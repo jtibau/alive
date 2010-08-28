a4d4504fea2caa66018a518b1ca0fb749359f832
Javier Tibau <jatb86@gmail.com>
2010-08-28 03:11:54 +0200

    forgot to commit PullManipulation

---
+++ interaction/PullManipulation/PullManipulation.pro
@@ -0,0 +1,15 @@
+TEMPLATE = lib
+CONFIG += debug
+INCLUDEPATH += $(ALIVE_HOME)/include
+INCLUDEPATH +=  $(VJ_BASE_DIR)/include/gmtl-0.6.0 \
+LIBS += -L$(ALIVE_HOME)/lib -lalive
+HEADERS = src/PullManipulation.h
+SOURCES = src/PullManipulation.cpp
+DEPENDPATH = src
+OBJECTS_DIR = build
+DESTDIR = lib
+target.path = $${PREFIX}/lib
+headers.files = $$HEADERS
+headers.path = $${PREFIX}/include/alive/interaction
+INSTALLS = target headers 
+
---
+++ interaction/PullManipulation/src/PullManipulation.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+ *
+ *	This library is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+
+#include "PullManipulation.h"
+
+namespace alive{
+	namespace interaction{
+		void PullManipulation::update(){
+			if( mInput->getButtonState(mButtonNumber) && mInput->getObjectSelectedFlag() ){
+					gmtl::Matrix44f objectMatrix = mInput->getSelectedObjectMatrix();
+
+					gmtl::Vec3f translation =  mInput->getWandDirection() * mInput->getTimeDelta();
+					translation *= 4;	// move faster/slower
+					translation[1] = 0.0f;	// comment out if you wanna fly!
+					// Post multiply the delta translation
+					gmtl::Matrix44f trans_matrix = objectMatrix * gmtl::makeTrans<gmtl::Matrix44f>(translation);
+
+					mInput->setSelectedTransformation(trans_matrix);
+					mInput->setApplyManipulation(true);
+				}	
+		}
+	}
+}
